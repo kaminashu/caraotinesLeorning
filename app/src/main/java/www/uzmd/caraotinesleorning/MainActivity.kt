b590b7421c8da0f977bd4770e2a33c8964d16b0e
kaminashu <ertertfd89werwe@mail.ru>
2023-10-16 19:37:54 +0500

    caraotines finished fast leorning

--- app/src/main/java/www/uzmd/caraotinesleorning/MainActivity.kt
+++ app/src/main/java/www/uzmd/caraotinesleorning/MainActivity.kt
@@ -2,10 +2,39 @@ package www.uzmd.caraotinesleorning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import www.uzmd.caraotinesleorning.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.dowlandBtn.setOnClickListener {
+            lifecycleScope.launch {
+                loadProgress()
+            }
+        }
     }
+
+    private suspend fun loadProgress() {
+        binding.progressBar.isVisible = true
+        binding.dowlandBtn.isEnabled=false
+        val salom = load()
+        binding.dowlandBtn.isEnabled=true
+        binding.progressBar.isVisible = false
+        binding.textView.text = salom
+    }
+
+    private suspend fun load(): String {
+        delay(2000)
+        return "salom"
+    }
+
 }
